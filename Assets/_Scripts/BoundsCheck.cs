df0f6b12d9563c92a202465d47aef19ccba0a499
mm2461 <43255719+mm2461@users.noreply.github.com>
2018-10-18 03:01:00 -0400

    s

--- Assets/_Scripts/BoundsCheck.cs
+++ Assets/_Scripts/BoundsCheck.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// To type the next 4 lines, start by typing /// and then Tab.
-/// <summary>
-/// Keeps a GameObject on screen.
-/// Note that this ONLY works for an orthographic Main Camera at [ 0, 0, 0 ].
-/// </summary>
+
 public class BoundsCheck : MonoBehaviour
-{ // a
+{ 
     [Header("Set in Inspector")]
     public float radius = 1f;
     [Header("Set Dynamically")]
     public float camWidth;
     public float camHeight;
+    private readonly Vector3 boundSize;
+
+    public object Vector3boundSize { get; private set; }
+
     void Awake()
     {
         camHeight = Camera.main.orthographicSize; // b
@@ -41,9 +42,14 @@ public class BoundsCheck : MonoBehaviour
     }
     // Draw the bounds in the Scene pane using OnDrawGizmos()
     void OnDrawGizmos()
-    { // e
+    { 
         if (!Application.isPlaying) return;
         Vector3boundSize = newVector3(camWidth * 2, camHeight * 2, 0.1f);
         Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+
+    private object newVector3(float v1, float v2, float v3)
+    {
+        throw new NotImplementedException();
     }
 }
